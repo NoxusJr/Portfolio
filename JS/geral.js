7c6bf214f3acb06e1a9dfacfea7f6a9a3aaf56f7
George Júnior <127257970+NoxusJr@users.noreply.github.com>
2023-09-08 19:16:54 -0300

    V3.1 - Responsivo & Estilo
    Add Div's Layers
    Add Dark Mode
    Add Chance Light To Dark Mode

--- JS/geral.js
+++ JS/geral.js
@@ -15,3 +15,9 @@ function ocultar(){
         btn.style.display = "none"
     }
 }
+
+// LIGHT TO DARK MODE
+
+function trocarModo(){
+    return document.documentElement.classList.toggle('dark')
+}
